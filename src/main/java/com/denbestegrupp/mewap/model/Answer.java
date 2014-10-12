210cef3f89ac29c83bae06a23c47b03c222dfd94
Oskar Nyberg <oskar@oskarnyberg.com>
2014-10-12 20:10:04 +0200

    Created a class representing a user and zes answers

---
+++ src/main/java/com/denbestegrupp/mewap/model/Answer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.denbestegrupp.mewap.model;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @author Oskar
+ */
+public class Answer extends HashMap<User, List<Date>> {
+
+    public Answer() {
+        super();
+    }
+        
+}
