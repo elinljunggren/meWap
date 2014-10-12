02cc741bb3d0937d77232444a629a74661200d8d
Oskar Nyberg <oskar@oskarnyberg.com>
2014-10-12 20:07:33 +0200

    Created user class

---
+++ src/main/java/com/denbestegrupp/mewap/model/User.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.denbestegrupp.mewap.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Oskar
+ */
+@Entity
+class User {
+    
+    @Id
+    private String email;
+    private String name;
+
+    public User() {
+    }
+    
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
