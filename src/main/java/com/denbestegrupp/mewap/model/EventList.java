8a5fdfa7032cb2c5f77da3d659fbc23aed3272e4
Emma Gustafsson <emma@dhcp-179027.eduroam.chalmers.se>
2014-10-13 14:00:20 +0200

    Added a file for EventList

---
+++ src/main/java/com/denbestegrupp/mewap/model/EventList.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.denbestegrupp.mewap.model;
+
+import com.denbestegrupp.mewap.persistence.AbstractDAO;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author emma
+ */
+@Stateless
+public class EventList extends AbstractDAO <Event,Long >{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    
+    public EventList(){
+        super(Event.class);
+    }
+    public List <Event> getByName(String name){
+        List <Event> found = new ArrayList<>();
+        for(Event e: findRange(0, count())){
+            if(e.getName().equals(name)){
+                found.add(e);
+            }
+        }
+        return found;
+    }
+    
+    @Override
+    protected EntityManager getEntityManager() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
