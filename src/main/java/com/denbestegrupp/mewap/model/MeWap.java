1f163d80bfeb0402b93d318a1ff56b4d80bc6658
Elin Ljunggren <elin@dhcp-180083.eduroam.chalmers.se>
2014-10-13 14:19:34 +0200

    Created interface and class for handling MeWap

---
+++ src/main/java/com/denbestegrupp/mewap/model/MeWap.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.denbestegrupp.mewap.model;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ *
+ * @author elin
+ */
+@ApplicationScoped
+public class MeWap implements IMeWap {
+    
+    @EJB
+    private IEventList eventList;
+    
+    protected MeWap() {
+        Logger.getAnonymousLogger().log(Level.INFO, "MeWap alive");
+    }
+
+    @Override
+    public IEventList getEventlist() {
+        return eventList;
+    }
+    
+    
+}
