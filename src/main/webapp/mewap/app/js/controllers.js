ddf152aa7169ef84870355a71b3d242010e54ccf
Josefin Ondrus <josefin.ondrus@gmail.com>
2014-10-15 16:35:16 +0200

    controller.js first version

--- src/main/webapp/mewap/app/js/controllers.js
+++ src/main/webapp/mewap/app/js/controllers.js
@@ -1,5 +1,36 @@
+'use strict';
+
 /* 
- * 
+ * @author Josefin Ondrus
  */
 
+var EventListControllers = angular.module('EventListControllers', []);
 
+EventListControllers.controller('EventListCtrl', ['$scope', 'EventListProxy',
+    function ($scope, EventListProxy) {
+        $scope.orderProp = 'id'; //Eventprop?!
+        $scope.pageSize = '10';
+        $scope.currentPage = 0;
+        EventListProxy.count()
+                .success(function (count) {
+                    $scope.count = count.value;
+                }).error(function () {
+            console.log("count: error");
+        });
+        getRange();
+        $scope.$watch('currentPage', function () {
+            getRange();
+        });
+        $scope.$watch('pageSize', function () {
+            getRange();
+        });
+        function getRange() {
+            var first = $scope.pageSize * $scope.currentPage;
+            EventListProxy.findRange(first, $scope.pageSize)
+                    .success(function (events) {
+                        $scope.products = events;
+                    }).error(function () {
+                console.log("findRange: error");
+            });
+        }
+    }]);
