20268b14651cbaba261fc70e0c5cecc935b9d26d
Oskar Nyberg <oskar@oskarnyberg.com>
2014-10-16 20:04:51 +0200

    Added ApplicationConfig for REST

---
+++ src/main/java/com/denbestegrupp/mewap/ApplicationConfig.java
@@ -0,0 +1,33 @@
+package com.denbestegrupp.mewap;
+
+
+
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ * Technical class needed by JAX-RS
+ * @author hajo
+ */
+@javax.ws.rs.ApplicationPath("webresources")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method.
+     * It is automatically populated with
+     * all resources defined in the project.
+     * If required, comment out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(com.denbestegrupp.mewap.auth.AuthResource.class);
+    }
+    
+}
