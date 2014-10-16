2b1c1aceba851ad98f1578073379307120493533
Oskar Nyberg <oskar@oskarnyberg.com>
2014-10-16 20:04:01 +0200

    Implemented test stuff

--- src/main/java/com/denbestegrupp/mewap/auth/GoogleAuth.java
+++ src/main/java/com/denbestegrupp/mewap/auth/GoogleAuth.java
@@ -5,12 +5,48 @@
  */
 package com.denbestegrupp.mewap.auth;
 
-
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Oskar
  */
 public class GoogleAuth {
+    
+    private static GoogleAuth instance = null;
+    
+    private GoogleAuth() {
+        
+    }
+    
+    public static GoogleAuth getInstance() {
+        instance = new GoogleAuth();
+        return instance;
+    }
+    
+    public URL getLoginURL() {
+        try {
+            return new URL("http://raksooo.se/logout");
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(GoogleAuth.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public URL getLogoutURL() {
+        try {
+            return new URL("http://raksooo.se/logout");
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(GoogleAuth.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    public boolean isLoggedIn() {
+        return false;
+    }
     
 }
