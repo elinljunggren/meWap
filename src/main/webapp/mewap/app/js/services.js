f5c99823ea03a62dada54adbc655075ce91ecc07
Josefin Ondrus <josefin.ondrus@gmail.com>
2014-10-17 15:31:11 +0200

    services and res. update

--- src/main/webapp/mewap/app/js/services.js
+++ src/main/webapp/mewap/app/js/services.js
@@ -11,7 +11,7 @@ eventListService.factory('EventListProxy', ['$http',
     function ($http) {
 
         //TODO
-        var url = 'http://localhost:8080/meWap/webresources/eventLists';
+        var url = 'http://localhost:8080/meWap/webresources/evens';
         return {
             findAll: function () {
                 return $http.get(url);
