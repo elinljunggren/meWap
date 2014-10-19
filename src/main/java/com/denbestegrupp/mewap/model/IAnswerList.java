efae3270d52a7e19807d6b59f0a58165f54c673c
Josefin Ondrus <josefin.ondrus@gmail.com>
2014-10-19 22:23:00 +0200

    created interface for answers

---
+++ src/main/java/com/denbestegrupp/mewap/model/IAnswerList.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.denbestegrupp.mewap.model;
+
+import com.denbestegrupp.mewap.persistence.IDAO;
+
+/**
+ *
+ * @author josefinondrus
+ */
+public interface IAnswerList extends IDAO<MWAnswer, Long>{
+    
+   // public List<MWAnswer> getByName(String name);
+    
+}
