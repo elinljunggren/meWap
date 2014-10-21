13c0ff449696c9172242fd324d62716f485dc525
Elin Ljunggren <elin.l.ljunggren@gmail.com>
2014-10-21 20:58:21 +0200

    Added arrows and pageindicator to image slider on start page

--- src/main/webapp/mewap/app/js/slider.js
+++ src/main/webapp/mewap/app/js/slider.js
@@ -5,60 +5,79 @@
  */
 
 var currentImage = 0;
-var images = ["img01.jpg", "img02.jpg", "img03.jpg", "img04.jpg"];
+var images;
 var timer;
-var interval = 2 * 1000;
+var interval = 3 * 1000;
+var pageIndicators = [];
 
 var goToImage = function(goTo) {
-    document.getElementById("sliderContents").style.left = "-" + goTo + "00%";
+    var sliderContents = document.getElementById("sliderContents");
+    if (sliderContents === null) {
+        return false;
+    }
+    sliderContents.style.left = "-" + goTo + "00%";
+    setActivePageIndicator(goTo);
     currentImage = goTo;
-}
+    return true;
+};
+
+var setActivePageIndicator = function(n) {
+    for (var i=0; i<pageIndicators.length; i++) {
+        pageIndicators[i].id = "";
+    }
+    pageIndicators[n].id = "active";
+};
 
 var nextImage = function() {
-    goToImage((currentImage + 1) % images.length);
-}
+    goToImage((currentImage + 1) % images);
+};
 
 var prevImage = function() {
     if(--currentImage < 0) {
-        currentImage = images.length - 1;
+        currentImage = images - 1;
     }   
     goToImage(currentImage);
-}
+};
 
-var animate = function() {
-    if(timer != null) {
+var startTimer = function() {
+    if(timer !== null) {
         clearTimeout(timer);
     }
     timer = setTimeout(function() {
         nextImage();
-        animate();
+        startTimer();
     }, interval);
-}
+};
 
-var generateImages = function() {
-    images.forEach(function(image) {
-        var li = document.createElement("li");
-        li.className = "slideli";
-        var img = document.createElement("img");
-        img.src = "images/" + image;
-        li.appendChild(img);
-        document.getElementById("sliderContents").appendChild(li);
-    });
-}
+var generatePageIndicat = function() {
+    for (var i=0; i<images; i++) {
+        var indicator = document.createElement("span");
+        indicator.setAttribute("n", i);
+        indicator.onclick = function() {
+            if (goToImage(this.getAttribute("n"))) {
+                startTimer();
+            }
+        };
+        pageIndicators[pageIndicators.length] = indicator;
+        document.getElementById("pageIndicators").appendChild(indicator)
+    }
+};
 
 var generateCSS = function() {
-    document.getElementById("sliderContents").style.width = images.length + "00%";
-    var lis = document.getElementsByClassName("slideli");
+    document.getElementById("sliderContents").style.width = images + "00%";
+    var lis = document.getElementById("sliderContents").getElementsByTagName("li");
     for (var i=0; i<lis.length; i++) {
-       lis[i].style.width = (100/images.length)  + "%";
+       lis[i].style.width = (100/images)  + "%";
     }
-}
+};
 
 var startSlide = function() {
-    generateImages();
+    images = document.getElementById("sliderContents").getElementsByTagName("li").length;
+    generatePageIndicat();
+    setActivePageIndicator(0);
     generateCSS();
-    animate();
-}
+    startTimer();
+};
 
 
 
